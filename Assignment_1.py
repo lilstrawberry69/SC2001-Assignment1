9b27b10d6b75027b368c4c9325e276d395fc1b2e
lilstrawberry69 <zhongmingfu0104@gmail.com>
2026-09-05 01:59:09 +0800

    Implement algorithm assignment solution

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,11 +1,13 @@
-def merge_sort(arr, S): #S is the value we are testing to finding out how far down we should split
+def merge_sort(arr, S):  # S is the value we are testing to finding out how far down we should split
     if len(arr) <= S:
-        insert_sort(arr) #adjusted this base case to perform insertion sort
+        insert_sort(arr)  # adjusted this base case to perform insertion sort
         return arr
     mid = len(arr) // 2
     left = merge_sort(arr[:mid])
     right = merge_sort(arr[mid:])
     return merge(left, right)
+
+
 def merge(left, right):
     result = []
     i = 0
@@ -16,16 +18,17 @@ def merge(left, right):
             i += 1
         else:
             result.append(right[j])
-            j+=1
+            j += 1
     result += left[i:]
     result += right[j:]
     return result
+
+
 def insert_sort(arr):
-    for i in range(1,len(arr) - 1):
-        j = i -1
-        while j >= 0 and arr[j] > arr[j+1]:
+    for i in range(1, len(arr) - 1):
+        j = i - 1
+        while j >= 0 and arr[j] > arr[j + 1]:
             temp = arr[j]
-            arr[j] = arr[j+1]
-            arr[j+1] = temp
-            j -=1
-    
+            arr[j] = arr[j + 1]
+            arr[j + 1] = temp
+            j -= 1
